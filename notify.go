ee68f89030667e9bccb97b9c15b0a23a125f34d7
yuanchang <yuanchang.xu@outlook.com>
2018-01-29 17:42:13 +0800

    add ref to write

--- notify.go
+++ notify.go
@@ -1,11 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"github.com/fsnotify/fsnotify"
 	"regexp"
 	"time"
+	"fmt"
 )
+
+type fileRef struct {
+	ref  int
+	file string
+}
+
+var fileRefMap map[string]*fileRef
 
 func StartListener() {
 	config := GetConfig()
@@ -33,22 +40,65 @@ func StartListener() {
 
 func ReadListener(event fsnotify.Event, config *Config) {
 	if event.Op&fsnotify.Write == fsnotify.Write {
-		ParseListener(event.Name, config)
+		// 存在文件引用
+		if fr := GetFileRefMap(event.Name); fr != nil {
+			// 达到计数值
+			if fr.ref >= 2 {
+				fmt.Println("开始写入：",fr.file)
+				ParseListener(*fr)
+				fmt.Println("写入完毕：",fr.file)
+				// 删除
+				DelFileRefMap(fr.file)
+				fmt.Println("删除完毕：",fr.file)
+			} else {
+				fmt.Println("增加引用计数：",fr.ref)
+				fr.AddFileRefValue()
+				fmt.Println("计数增加完毕：",fr.ref)
+			}
+		} else {
+			fmt.Println("设置fileRef：",fr.file)
+			SetFileRefMap(event.Name)
+		}
 	}
 }
 
-func ParseListener(file string, config *Config) {
-	fmt.Println("modify file: ", file)
+func ParseListener(fr fileRef) {
 
-	if matched, err := regexp.MatchString(`match[0-9]+\.json$`, file); matched && err == nil {
+	if matched, err := regexp.MatchString(`match[0-9]+\.json$`, fr.file); matched && err == nil {
 		time.AfterFunc(time.Second*3, func() {
-			ParseMatchSave(file)
+			ParseMatchSave(fr.file)
 		})
 	}
 
-	if matched, err := regexp.MatchString(`market[0-9]+\.json`, file); matched && err == nil {
+	if matched, err := regexp.MatchString(`market[0-9]+\.json`, fr.file); matched && err == nil {
 		time.AfterFunc(time.Second*3, func() {
-			ParseMarketSave(file)
+			ParseMarketSave(fr.file)
 		})
 	}
 }
+
+
+func (fr *fileRef )AddFileRefValue()  {
+	fr.ref++
+}
+
+func GetFileRefMap(file string) (*fileRef) {
+	if elem, ok := fileRefMap[file]; ok {
+		return elem
+	}
+	return nil
+}
+
+func SetFileRefMap(file string) {
+	fr := &fileRef{
+		file: file,
+		ref:  1,
+	}
+	fileRefMap[file] = fr
+}
+
+func DelFileRefMap(file string) {
+	delete(fileRefMap, file)
+}
+
+
